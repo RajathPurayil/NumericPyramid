d040263062533c311a0f8729ff0472117fbbe7f8
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-27 00:13:30 -0400

    update commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int count =1;
+        for (int i=1;i<=4;i++)
+
+        {
+            for (int j = 4;j>=i;j--)
+
+            System.out.println("");
+
+            for (int k=1; k<=i;k++)
+                System.out.print(i +"");
+
+            }
+        System.out.println();
+
     }
 }
